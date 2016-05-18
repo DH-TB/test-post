a67cb0fb953ce954d5829c21e90819d446b37c68
DH-TB <929659475@qq.com>
2016-05-18 13:08:14 +0800

    src/

---
+++ src/collect_all_even.js
@@ -0,0 +1,26 @@
+function collect_same_elements(collection_a, object_b) {
+
+    return buildSameElement(collection_a, object_b);
+}
+
+function buildSameElement(collection_a, object_b){
+    var same = [];
+
+    collection_a.forEach(function (coll_a) {
+        same = buildA(same,coll_a,object_b);
+    });
+
+    return same;
+}
+
+function buildA(same,coll_a,object_b) {
+    for (var i = 0; i < object_b.value.length; i++) {
+        if (coll_a.key === object_b.value[i]) {
+            same.push(coll_a.key);
+        }
+    }
+
+    return same;
+}
+
+module.exports = collect_same_elements;
